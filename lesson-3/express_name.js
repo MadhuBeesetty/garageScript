30017997749fd328bd2d69ca1cfab4172f4e9711
Madhu <madhub@llip.life>
2017-08-18 11:03:27 -0700

    express_name

--- lesson-3/express_name.js
+++ lesson-3/express_name.js
@@ -2,14 +2,14 @@ const fs =require("fs");
 const express =require("express");
 const app =express();
 app.listen(3610);
-const prog =(re1,re2)=>{
+const prog =(req,res)=>{
   const name =(err,files)=>{
     let list = " ";
     files.forEach((user)=>{
       if (user !="madhub")
         list =list + " " + user;
       });
-    re2.send(list);
+    res.send(list);
   };
 fs.readdir('/home',name);
 };
