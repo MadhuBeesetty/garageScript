d2893793176004000d56d659f427da75b017902d
Madhu <madhub@llip.life>
2017-08-17 19:54:35 -0700

    names on server commit

--- lesson-3/express_name.js
+++ lesson-3/express_name.js
@@ -1,12 +1,17 @@
-const a =require("fs");
+const fs =require("fs");
 const express =require("express");
 const app =express();
 app.listen(3610);
 const prog =(re1,re2)=>{
-    const name =(err,files)=>{
-    re2.send(files);
+  const name =(err,files)=>{
+    let list = " ";
+    files.forEach((user)=>{
+      if (user !="madhub")
+        list =list + " " + user;
+      });
+    re2.send(list);
   };
-a.readdir('/home',name);
+fs.readdir('/home',name);
 };
 app.get('/madhu',prog);
 
