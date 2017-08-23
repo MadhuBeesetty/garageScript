5525af588e1b0f751f2cf6f9d419b73003303233
Madhu <madhub@llip.life>
2017-08-22 22:36:48 -0700

    ajax final edits

--- lesson-5/ajax.js
+++ lesson-5/ajax.js
@@ -6,6 +6,6 @@ app.listen(3610);
 app.use(express.static('public'));
 
 app.get("/XMLHttpRequest",(req,res) => {
-  fs.appendFile('/home/madhub/garagescript/lesson-5/public/text.txt', `\n Name: ${req.query.a1} Comment:${req.query.a2} \n`);
+  fs.appendFile('/home/madhub/garagescript/lesson-5/public/text.txt', `\n Name: ${req.query.nameInput} Comment:${req.query.commentInput} \n`);
   res.send('Your comment has been received.');
 });
