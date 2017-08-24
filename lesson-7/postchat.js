eb6ebd81fb937914fd5ad2d94da452b9fc704deb
Madhu <madhub@llip.life>
2017-08-23 22:59:11 -0700

    creating a chat application using post

---
+++ lesson-7/postchat.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const fs = require("fs");
+const app =express();
+const bodyparser =require("body-parser");
+app.listen(3610);
+app.use(express.static('public'));
+app.use(bodyparser.json());
+
+app.post('/submit',(req,res) => {
+  fs.appendFile('/home/madhub/garagescript/lesson-7/public/notes.txt',`/n name: ${req.body.name} comment: ${req.body.comment} \n`);
+  res.send('got the post request');
+});
+
