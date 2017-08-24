68b460a14daad7e1d3eb89446eb4692af368d7d3
Madhu <madhub@llip.life>
2017-08-24 15:35:55 -0700

    lesson-6 one last final commit

--- lesson-6/chat.js
+++ lesson-6/chat.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const app = express();
 app.listen(3610);
 app.use(express.static('public'));
-app.get('/XMLHttpRequest', (req,res) =>{
+app.get('/madhu', (req,res) =>{
   fs.appendFile('/home/madhub/garagescript/lesson-6/public/notes.txt',` name: ${req.query.myName} comment: ${req.query.myComment}`);
   res.send("your comment have been received");
   });
