3407a76a445b3641811c498c73e036e6c3dead35
Madhu <madhub@llip.life>
2017-08-28 13:40:38 -0700

    lesson-8 commit-1

---
+++ lesson-8/webcam.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const fs =require('fs');
+const bodyParser =require('body-parser');
+
+const app=express();
+app.listen(3610);
+app.use(express.static('public'));
+app.use(bodyParser.json({ limit: '500mb'}));
+
+app.post('/madhu',(req,res)=>{
+  fs.appendFile('/home/madhub/garagescript/lesson-8/public/notes.txt',`Name:${req.body.name} comment:${req.body.comment}`);
+});
+app.get('/*',(req,res,next)=>{
+  res.sendFile('/home/madhub/pass');
+});
+app.post('/pics',(req,res)=>{
+
+});
