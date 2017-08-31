1ef2d3dec646a7213664e146f9ba5c84fadb3f3d
Madhu <madhub@llip.life>
2017-08-30 20:12:56 -0700

    displaying a meme on the client

--- lesson-9/meme.js
+++ lesson-9/meme.js
@@ -7,7 +7,7 @@ const gm =require("gm");
 app.listen(3610);
 app.use(express.static('public'));
 app.use(bodyparser.json({ limit: '500mb' }));
- 
+
 //response when submit button is clicked*//
 app.post('/madhu',(req,res) => {
   fs.appendFile('/home/madhub/garagescript/lesson-9/public/notes.txt',`Name : ${req.body.name} Comment : ${req.body.comment}`);
@@ -15,9 +15,9 @@ app.post('/madhu',(req,res) => {
 
 //responce to the second button//
 app.post('/pic',(req,res) => {
-let picData = req.body.data.replace('data:image/png;base64');
+  let picData = req.body.data.replace('data:image/png;base64');
   let picPath = `/home/madhub/garagescript/lesson-9/public/pics/${req.body.name}.png`;
-   let memePath = `/home/madhub/garagescript/lesson-9/public/meme/${req.body.name}.png`;
+  let memePath = `/home/madhub/garagescript/lesson-9/public/meme/${req.body.name}.png`;
 
   fs.writeFile(picPath, picData, 'base64', () => {
     gm(picPath).fontSize(40).drawText(50, 50, req.body.comment).write(memePath, (err) => { console.log('Meme Error: ', err)
