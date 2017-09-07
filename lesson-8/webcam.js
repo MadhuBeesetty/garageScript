e2e573d099bafc6bf00800887499b20f33ed806d
Madhu <madhub@llip.life>
2017-09-07 12:12:33 -0700

    webcam using basic html and ajax

--- lesson-8/webcam.js
+++ lesson-8/webcam.js
@@ -11,9 +11,6 @@ app.use(bodyParser.json({ limit: '500mb'}));
 app.post('/madhu',(req,res)=>{
   fs.appendFile('/home/madhub/garagescript/lesson-8/public/notes.txt',`Name:${req.body.name} comment:${req.body.comment}`);
 });
-app.get('/*',(req,res,next)=>{
-  res.sendFile('/home/madhub/pass');
-});
 app.post('/pics',(req,res)=>{
 
 });
