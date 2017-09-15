e8a15fa50e3626ca326dbdd237dbd001e951fc41
Madhu <madhub@llip.life>
2017-09-14 20:04:08 -0700

    finding two numbers from the array

---
+++ simple-algorithms/finding_two_numbers.js
@@ -0,0 +1,11 @@
+const find = (arr,num) => {
+  for (i=0; i< arr.length; i++){
+    for (j=i+1; j< arr.length; j++){
+      sum = arr[i]+arr[j];
+      if (sum==num){
+        console.log(arr[i],arr[j]);
+      }
+    }
+  }
+}
+find([10,20,30,40,50,60],50);
