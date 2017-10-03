29e1597dd372f904a29fb0a9cbce46f52c6dae34
Madhu <madhub@llip.life>
2017-10-02 18:57:08 -0700

    simple algorithms

---
+++ simple-algorithms/callsmallfunction.js
@@ -0,0 +1,17 @@
+const func =()=>{
+  console.log("madhu")
+}
+const name = (a,b,d,c,i=1)=>{
+  if(a<b){
+    c=a
+  }
+  else{
+    c=b
+  }
+  func();
+  if( c > i ){
+    return;
+  }
+  return name(a,b,d,c,i+1);
+}
+name(2,3,func())
